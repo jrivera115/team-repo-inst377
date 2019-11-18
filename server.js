fed3500f29ceb9c358158c609ce5392f3514295e
Jonah Lisa <rivera.jonah@ymail.com>
2019-11-18 11:54:08 -0500

    Added process.env.PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
