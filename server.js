f7d7a33c2cef7cd17349daf57dcbe9b357711e41
Jonah Lisa <rivera.jonah@ymail.com>
2019-12-04 21:15:23 -0500

    Added POST and PUT endpoints

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
 const fetch = require('node-fetch');
 
@@ -7,40 +8,13 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-/*
- * The 'express.static' middleware provides some services Express can use to
- * serve files from a directory - in this case, the 'public' subdirectory of
- * this project.
- *
- * The 'app.use' function attaches middleware to our Express application, so
- * that when the application is running, it can serve static files. In this
- * case, we mount it over the entire app: any web request that GETs a path that
- * exists in the 'public' directory will be handled by the middleware. The
- * middleware also serves the 'index.html' file in a directory (if it exists)
- * whenever a client requests the directory itself.
- *
- * The 'public' directory for this project, in turn, contains all the HTML,
- * Javascript, and CSS files needed to run a simple chat client connected to
- * this server. Accessing this server's root URL will serve 'public/index.html',
- * which contains our chat client. This gives users an easy way to connect to
- * the server and interact with other users.
- *
- * See also:
- *  - Express: Serving static files in Express
- *    https://expressjs.com/en/starter/static-files.html
- *  - Express: express.static()
- *    https://expressjs.com/en/4x/api.html#express.static
- *  - Express: Using middleware
- *    https://expressjs.com/en/guide/using-middleware.html
- *  - Express: app.use()
- *    https://expressjs.com/en/4x/api.html#app.use
- */
+// POST middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static('src'));
 
-// this is a single route, in the simplest possible format
-// the simplest format is not necessarily the best one.
-// this is, right now, an introduction to Callback Hell
-// but it is okay for a first-level example
 app.get('/api', (req, res) => {
   const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
   fetch(baseURL)
@@ -48,7 +22,6 @@ app.get('/api', (req, res) => {
     .then((r) => r.map((dataset) => dataset))
     .then((data) => {
       console.log(data);
-      // res.render(index_1.html);
       res.send({ data: data });
     })
     .catch((err) => {
@@ -57,4 +30,13 @@ app.get('/api', (req, res) => {
     });
 });
 
+app.post('/api', (req, res) => {
+  res.send('incoming POST request!');
+});
+
+app.put('/api', (req, res) => {
+  res.send('incoming PUT request!');
+});
+
+// eslint-disable-next-line max-len
 app.listen(port, () => console.log(`Our app is running on https://safetyzone-demo.herokuapp.com/ and port: ${port}!`));
