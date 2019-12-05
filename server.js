ceb1b83942bdf6e896784d27758965fd09232361
Jonah Lisa <rivera.jonah@ymail.com>
2019-12-04 21:54:35 -0500

    Testing with Postman and different endpoints

--- server.js
+++ server.js
@@ -1,50 +1,24 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
 const fetch = require('node-fetch');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser'); // for post req
+//const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: false}));
+// POST REQUEST
+app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
-/*
- * The 'express.static' middleware provides some services Express can use to
- * serve files from a directory - in this case, the 'public' subdirectory of
- * this project.
- *
- * The 'app.use' function attaches middleware to our Express application, so
- * that when the application is running, it can serve static files. In this
- * case, we mount it over the entire app: any web request that GETs a path that
- * exists in the 'public' directory will be handled by the middleware. The
- * middleware also serves the 'index.html' file in a directory (if it exists)
- * whenever a client requests the directory itself.
- *
- * The 'public' directory for this project, in turn, contains all the HTML,
- * Javascript, and CSS files needed to run a simple chat client connected to
- * this server. Accessing this server's root URL will serve 'public/index.html',
- * which contains our chat client. This gives users an easy way to connect to
- * the server and interact with other users.
- *
- * See also:
- *  - Express: Serving static files in Express
- *    https://expressjs.com/en/starter/static-files.html
- *  - Express: express.static()
- *    https://expressjs.com/en/4x/api.html#express.static
- *  - Express: Using middleware
- *    https://expressjs.com/en/guide/using-middleware.html
- *  - Express: app.use()
- *    https://expressjs.com/en/4x/api.html#app.use
- */
+
 app.use(express.static('src'));
 
-// this is a single route, in the simplest possible format
-// the simplest format is not necessarily the best one.
-// this is, right now, an introduction to Callback Hell
-// but it is okay for a first-level example
+
 app.get('/api', (req, res) => {
-  const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  // eslint-disable-next-line max-len
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json$$app_token=9KQOpcS5cOuViblS4ca1bkM4j';
   fetch(baseURL)
     .then((r) => r.json())
     .then((r) => r.map((dataset) => dataset))
@@ -58,5 +32,22 @@ app.get('/api', (req, res) => {
     });
 });
 
+app.post('/api/post', (req, res) => {
+  res.json({
+    message: 'POST!!!!'
+  });
+});
+// PUT request
+app.put('/api/put', (req, res) => {
+  res.json({
+    message: 'PUT!!!!'
+  });
+});
 
+// create a token
+//app.post('/api/login', (req, res) => {
+ // jwt.sign({ foo: 'bar' }, cert, { algorithm });
+//});
+
+// eslint-disable-next-line max-len
 app.listen(port, () => console.log(`Our app is running on https://safetyzone-demo.herokuapp.com/ and port: ${port}!`));
