56b68d737dde1df4b4849f08b5e5002f9a9a7d1c
Jonah Rivera <54821166+jrivera115@users.noreply.github.com>
2019-12-05 22:34:17 -0500

    Added POST & PUT endpoints
    End points do not work because token authentication code is missing.

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 /* eslint-disable linebreak-style */
 const express = require('express');
 const fetch = require('node-fetch');
-
+const bodyParser = require('body-parser'); // for post req
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// POST REQUEST
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static('src'));
 
 app.get('/api', (req, res) => {
+  // eslint-disable-next-line max-len
   const baseURL = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
   fetch(baseURL)
     .then((r) => r.json())
@@ -23,6 +28,18 @@ app.get('/api', (req, res) => {
       console.log(err);
       res.redirect('/error');
     });
+});
+
+app.post('/api/post', (req, res) => {
+  res.json({
+    message: 'POST!!!!'
+  });
+});
+
+app.put('/api/put', (req, res) => {
+  res.json({
+    message: 'PUT!!!!'
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
